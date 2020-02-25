eb22ff98d141d14ef7a332b5668eaaf9b5594510
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-02-25 09:05:16 -0300

    Learning js

---
+++ basics/types.js
@@ -0,0 +1,10 @@
+//number
+console.log(12%5) //module
+console.log('this isn\'t ')
+console.log("string1"+"string2")
+console.log(10+"34") // -> 1034
+console.log(10-3) // -> 7
+console.log('hello' - "bye") // -> NaN
+console.log(10 > 3)
+console.log(10 === 10) //to compare two things
+console.log(3 !== 3) //different
